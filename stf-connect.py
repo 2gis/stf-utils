ff303bcca44cbeb1fa35b9de8fc3198a5e9de17c
Rozov Arseniy <a.rozov@2Gis.ru>
2016-02-11 19:38:54 +0600

    add delete method

--- stf-connect.py
+++ stf-connect.py
@@ -28,6 +28,17 @@ def get_available_devices(api_url, oauth_token):
     return device_list
 
 
+def get_owned_devices(api_url, oauth_token):
+    url = "{0}{1}".format(api_url, user_devices_path)
+    resp = requests.get(
+        url,
+        headers={
+            "Authorization": "Bearer {0}".format(oauth_token)
+        }
+    )
+    return resp.json().get("devices")
+
+
 def add_all_devices(api_url, oauth_token):
     device_list = get_available_devices(api_url, oauth_token)
     url = "{0}{1}".format(api_url, user_devices_path)
@@ -47,14 +58,12 @@ def add_all_devices(api_url, oauth_token):
 
 
 def delete_all_devices(api_url, oauth_token):
-    serial_list = get_available_devices(api_url, oauth_token)
-    url = "{0}{1}".format(api_url, user_devices_path)
-    for device_serial in serial_list:
-        resp = requests.post(
+    device_list = get_owned_devices(api_url, oauth_token)
+    for device in device_list:
+        device_serial = device.get("serial")
+        url = "{0}{1}/{2}".format(api_url, user_devices_path, device_serial)
+        resp = requests.delete(
             url,
-            data=dumps({
-                "serial": "{0}".format(device_serial)
-            }).encode("utf-8"),
             headers={
                 "Content-Type": "application/json",
                 "Authorization": "Bearer {0}".format(oauth_token)
@@ -63,3 +72,4 @@ def delete_all_devices(api_url, oauth_token):
         log.info(loads(resp.text).get("description"))
 
 add_all_devices(API_URL, OAUTH_TOKEN)
+delete_all_devices(API_URL, OAUTH_TOKEN)
