b7e51fa0abc558f9f8c51eabef26577e2e864063
Gleb Golovin <g.golovin@2gis.ru>
2016-05-24 12:27:09 +0600

    Add timeout on adb command execution

--- common/adb.py
+++ common/adb.py
@@ -2,15 +2,17 @@ import os
 import subprocess
 import logging
 import time
+from threading import Timer
 
 log = logging.getLogger(__name__)
 WAIT_FOR_CONNECT = 5
+ADB_TIMEOUT = 10
 
 
 def connect(connect_url):
     log.debug("ADB start connecting %s" % connect_url)
     command = ["connect", connect_url]
-    stdout, stderr = _exec_adb(command).communicate()
+    stdout, stderr = _exec_adb(command)
     start_time = time.time()
     while True:
         time.sleep(1)
@@ -31,7 +33,7 @@ def device_is_ready(device_adb_name):
 
 def get_state(device_adb_name):
     command = "-s %s get-state" % device_adb_name
-    stdout, stderr = _exec_adb(command.split()).communicate()
+    stdout, stderr = _exec_adb(command.split())
     log.debug("ADB getting state of device %s. \nStdout %s. \nStderr %s." % (device_adb_name, stdout, stderr))
     return stdout, stderr
 
@@ -39,16 +41,27 @@ def get_state(device_adb_name):
 def shutdown_emulator(connect_url):
     emulator_shell = '-s %s shell' % connect_url
     shutdown_command = "am start -a android.intent.action.ACTION_REQUEST_SHUTDOWN; stop adbd"
-    stdout, stderr = _exec_adb(emulator_shell.split() + [shutdown_command]).communicate()
+    stdout, stderr = _exec_adb(emulator_shell.split() + [shutdown_command])
     log.debug("ADB shutdown emulator %s. \nStdout %s. \nStderr %s" % (connect_url, stdout, stderr))
 
 
 def _exec_adb(params):
     command = ['adb'] + params
     log.debug("Executing adb command: %s" % command)
-    return subprocess.Popen(
+    process = subprocess.Popen(
         command,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         env=os.environ.copy()
     )
+    timer = Timer(ADB_TIMEOUT, _kill_process, [process])
+    try:
+        timer.start()
+        return process.communicate()
+    finally:
+        timer.cancel()
+
+
+def _kill_process(process):
+    log.error("Execution timeout expired. Kill %s process." % process.pid)
+    process.kill()
