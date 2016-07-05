ff74f6e0aa5b009a1904f4c9ad71a07d8fa54d97
sh0ked <nikolay.ustinov90@gmail.com>
2016-07-05 17:50:51 +0600

    connect-new-devices-after-device-lost

--- common/adb.py
+++ common/adb.py
@@ -9,32 +9,47 @@ WAIT_FOR_CONNECT = 5
 ADB_TIMEOUT = 10
 
 
-def connect(connect_url):
-    log.debug("ADB start connecting %s" % connect_url)
-    command = ["connect", connect_url]
+def command_with_wait(command, connect_url):
+    log.debug("ADB start command %s for %s" % (command, connect_url))
     stdout, stderr = _exec_adb(command)
     start_time = time.time()
     while True:
         time.sleep(1)
         if device_is_ready(connect_url):
-            log.debug("ADB device %s connected. \nStdout %s. \nStderr %s." % (connect_url, stdout, stderr))
+            log.debug("ADB command %s for device %s was successful. "
+                      "Stdout %s. Stderr %s." % (command, connect_url, stdout, stderr))
             break
         elif time.time() - start_time > WAIT_FOR_CONNECT:
-            raise OSError("ADB connect for device %s has been failed" % connect_url)
+            raise OSError("ADB command %s for device %s has been failed" % (command, connect_url))
+
+
+def connect(connect_url):
+    command = ["connect", connect_url]
+    command_with_wait(command, connect_url)
+
+
+def disconnect(connect_url):
+    log.debug("ADB start disconnecting %s" % connect_url)
+    command = ["disconnect", connect_url]
+    stdout, stderr = _exec_adb(command)
+    log.debug("ADB disconnect for device %s was done. "
+              "Stdout %s. Stderr %s." % (connect_url, stdout, stderr))
 
 
 def device_is_ready(device_adb_name):
-    ready = True
     state, stderr = get_state(device_adb_name)
     if state != b'device\n':
-        ready = False
-    return ready
+        log.debug("Device %s isn't ready and has status is %s" % (device_adb_name, state))
+        return False
+    else:
+        log.debug("Device %s is ready" % device_adb_name)
+        return True
 
 
 def get_state(device_adb_name):
     command = "-s %s get-state" % device_adb_name
     stdout, stderr = _exec_adb(command.split())
-    log.debug("ADB getting state of device %s. \nStdout %s. \nStderr %s." % (device_adb_name, stdout, stderr))
+    log.debug("ADB getting state of device %s. Stdout %s. Stderr %s." % (device_adb_name, stdout, stderr))
     return stdout, stderr
 
 
@@ -42,7 +57,7 @@ def shutdown_emulator(connect_url):
     emulator_shell = '-s %s shell' % connect_url
     shutdown_command = "am start -a android.intent.action.ACTION_REQUEST_SHUTDOWN; stop adbd"
     stdout, stderr = _exec_adb(emulator_shell.split() + [shutdown_command])
-    log.debug("ADB shutdown emulator %s. \nStdout %s. \nStderr %s" % (connect_url, stdout, stderr))
+    log.debug("ADB shutdown emulator %s. Stdout %s. Stderr %s" % (connect_url, stdout, stderr))
 
 
 def _exec_adb(params):
--- tests/test_stf_connect_client.py
+++ tests/test_stf_connect_client.py
@@ -3,16 +3,20 @@ from mock import patch, Mock
 from unittest import TestCase
 
 from tests.helpers import get_response_from_file, wait_for
-from stf_connect.client import SmartphoneTestingFarmClient
+from stf_connect.client import SmartphoneTestingFarmClient, STFConnectedDevicesWatcher
 
 
 class TestSmartphoneTestingFarmClient(TestCase):
     def setUp(self):
         super(TestSmartphoneTestingFarmClient, self).setUp()
         self.watcher = None
-        self.get_all_devices = get_response_from_file('get_all_devices.json')
-        self.get_device = get_response_from_file('get_device_x86.json')
-        self.remote_connect = get_response_from_file('remote_connect.json')
+        get_all_devices = get_response_from_file('get_all_devices.json')
+        get_device = get_response_from_file('get_device_x86.json')
+        remote_connect = get_response_from_file('remote_connect.json')
+
+        self.all_devices_mock = Mock(return_value=Mock(json=Mock(return_value=get_all_devices)))
+        self.get_device_mock = Mock(return_value=Mock(json=Mock(return_value=get_device)))
+        self.remote_connect_mock = Mock(return_value=Mock(json=Mock(return_value=remote_connect)))
 
     def tearDown(self):
         if self.watcher:
@@ -30,18 +34,24 @@ class TestSmartphoneTestingFarmClient(TestCase):
         ]
     ])
     def test_connect_devices(self, device_spec):
-        all_devices_mock = Mock(return_value=Mock(json=Mock(return_value=self.get_all_devices)))
-        get_device_mock = Mock(return_value=Mock(json=Mock(return_value=self.get_device)))
-        remote_connect_mock = Mock(return_value=Mock(json=Mock(return_value=self.remote_connect)))
+        """
+        - set config with 1 device
+        - try to connect devices
 
+        Expected: 1 device connected and 1 device in connected_devices list
+
+        - stop stf-connect
+
+        Expected: 0 devices connected and lists of devices was empty
+        """
         with patch(
-            'stf_connect.client.SmartphoneTestingFarmClient.get_all_devices', all_devices_mock,
+            'stf_connect.client.SmartphoneTestingFarmClient.get_all_devices', self.all_devices_mock,
         ), patch(
-            'stf_connect.client.SmartphoneTestingFarmClient.get_device', get_device_mock,
+            'stf_connect.client.SmartphoneTestingFarmClient.get_device', self.get_device_mock,
         ), patch(
             'stf_connect.client.SmartphoneTestingFarmClient.add_device', Mock(),
         ), patch(
-            'stf_connect.client.SmartphoneTestingFarmClient.remote_connect', remote_connect_mock,
+            'stf_connect.client.SmartphoneTestingFarmClient.remote_connect', self.remote_connect_mock,
         ), patch(
             'stf_connect.client.SmartphoneTestingFarmClient.delete_device', Mock(),
         ), patch(
@@ -50,6 +60,8 @@ class TestSmartphoneTestingFarmClient(TestCase):
             'common.adb.device_is_ready', Mock(return_value=True)
         ), patch(
             'common.adb.connect', Mock(return_value=True)
+        ), patch(
+            'common.adb.disconnect', Mock(return_value=True)
         ):
             stf = SmartphoneTestingFarmClient(
                 host="http://host.domain",
@@ -69,3 +81,72 @@ class TestSmartphoneTestingFarmClient(TestCase):
 
             wait_for(lambda: self.assertEqual(len(stf.device_groups[0].get("added_devices")), 0))
             wait_for(lambda: self.assertEqual(len(stf.device_groups[0].get("connected_devices")), 0))
+
+    @dataprovider([
+        [
+            {
+                "group_name": "alfa",
+                "amount": "1",
+                "specs": {"abi": "x86"}
+            }
+        ]
+    ])
+    def test_connect_new_device_after_device_lost(self, device_spec):
+        """
+        - set config with 1 device
+        - try to connect devices
+
+        Expected: 1 device connected and 1 device in connected_devices list
+
+        - start devices watcher
+        - got 'False' in device_is_ready method (connected device is not available)
+
+        Expected: 0 devices connected and lists of devices was empty
+        (device was removed from stf-connect and device by adb was disconnected)
+
+        - try to connect available devices
+
+        Expected: 1 device connected and 1 device in connected_devices list
+        """
+        def raise_exception():
+            raise Exception('something ugly happened in adb connect')
+
+        with patch(
+            'stf_connect.client.SmartphoneTestingFarmClient.get_all_devices', self.all_devices_mock,
+        ), patch(
+            'stf_connect.client.SmartphoneTestingFarmClient.get_device', self.get_device_mock,
+        ), patch(
+            'stf_connect.client.SmartphoneTestingFarmClient.add_device', Mock(),
+        ), patch(
+            'stf_connect.client.SmartphoneTestingFarmClient.remote_connect', self.remote_connect_mock,
+        ), patch(
+            'common.adb.device_is_ready', Mock(side_effect=[False, True, True])
+        ), patch(
+            'common.adb.connect', Mock(side_effect=[True, raise_exception, True])
+        ), patch(
+            'common.adb.disconnect', Mock(return_value=True)
+        ):
+            stf = SmartphoneTestingFarmClient(
+                host="http://host.domain",
+                common_api_path="/api/v1",
+                oauth_token="test token",
+                device_spec=device_spec,
+                devices_file_path="./devices",
+                shutdown_emulator_on_disconnect=True
+            )
+            stf.connect_devices()
+
+            self.assertTrue(wait_for(lambda: len(stf.device_groups[0].get("added_devices")) == int(device_spec[0].get("amount"))))
+            self.assertTrue(wait_for(lambda: len(stf.device_groups[0].get("connected_devices")) == int(device_spec[0].get("amount"))))
+
+            self.watcher = STFConnectedDevicesWatcher(stf)
+            self.watcher.start()
+
+            self.assertTrue(wait_for(lambda: len(stf.device_groups[0].get("added_devices")) == 0))
+            self.assertTrue(wait_for(lambda: len(stf.device_groups[0].get("connected_devices")) == 0))
+
+            stf.connect_devices()
+
+            self.assertTrue(wait_for(lambda: stf.shutdown_emulator_on_disconnect))
+            self.assertTrue(wait_for(lambda: len(stf.device_groups[0].get("added_devices")) == int(device_spec[0].get("amount"))))
+            self.assertTrue(wait_for(lambda: len(stf.device_groups[0].get("connected_devices")) == int(device_spec[0].get("amount"))))
