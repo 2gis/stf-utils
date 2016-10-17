b82a7f2250fdb81be9dbd9049659459661f528b1
Розов Арсений Николаевич <a.rozov@2gis.ru>
2016-10-17 19:48:53 +0700

    update requests to 2.10.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     },
     install_requires=[
         'six==1.10.0',
-        'requests==2.9.1',
+        'requests==2.10.0',
         'asyncio==3.4.3',
         'autobahn==0.13.0',
     ],
