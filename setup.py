260fca2ab97bd7d8650b05ec2b16df25fbc380b4
Stanislav Zubov <z00sts@users.noreply.github.com>
2017-08-07 12:08:04 +0700

    do-not-try-to-stop-not-running-threads (#37)
    * do-not-try-to-stop-not-running-threads

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
             'config/*.ini'
         ],
     },
-    version='0.0.4',
+    version='0.0.5',
     entry_points={
         'console_scripts': [
             'stf-connect = stf_utils.stf_connect.stf_connect:run',
