dc130576f858d0c6d682867c37cf884f90d71609
s.zubov <s.zubov@2Gis.ru>
2017-08-16 12:04:52 +0700

    connect-and-stop-mode/gracefully-exit-with-error-code

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
             'config/*.ini'
         ],
     },
-    version='0.1.3',
+    version='0.1.4',
     entry_points={
         'console_scripts': [
             'stf-connect = stf_utils.stf_connect.stf_connect:run',
